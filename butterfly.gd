b9a7ace1c4fe86d1e88a05c8f1e6dcb1ac2310dd
Nikolaj Baer <nikolaj.baer@gmail.com>
2025-10-04 14:14:51 -0700

    fly and catch basics

--- butterfly.gd
+++ butterfly.gd
@@ -1,42 +1,62 @@
-extends CharacterBody3D
-
-const GRAVITY = Vector3(0,-0.1,0)
-const UP = Vector3(0,0.3,0)
+extends Area3D
+const GRAVITY = Vector3(0,-1,0)
+const UP = Vector3(0,3,0)
 
 @onready var lwing = $ButterflyWing2
 @onready var rwing = $ButterflyWing
 
 @export var wing_color : Color = Color("red")
 
+const MAX_DIST_FROM_START = 1
+const SPEED = -0.75
+
+var init_pos:Vector3
 var flap_tween:Tween
+var velocity: Vector3
+var low_bound: float 
+var flapping: bool = false
+var caught:bool = false
+var rotate_speed:float = 0
 
 func _ready():
+	rotate_speed = deg_to_rad(45 - randf()*90)
+	velocity = Vector3()
+	init_pos = self.position
+	low_bound = init_pos.y - randf() * 1.5
 	$ButterflyWing/wing_mesh.material_override.albedo_color = wing_color
 	$ButterflyWing2/wing_mesh.material_override.albedo_color  = wing_color
 
 func flap_down():
+	flapping = true
 	if flap_tween: flap_tween.cancel_free()
 	flap_tween = get_tree().create_tween()
-	flap_tween.tween_property(lwing,"rotation",Vector3(lwing.rotation.x,lwing.rotation.y,deg_to_rad(220)),0.25)
+	flap_tween.tween_property(lwing,"rotation",Vector3(lwing.rotation.x,lwing.rotation.y,deg_to_rad(220)),0.5)
 	flap_tween.set_parallel()
-	flap_tween.tween_property(rwing,"rotation",Vector3(rwing.rotation.x,rwing.rotation.y,deg_to_rad(-40)),0.25)
+	flap_tween.tween_property(rwing,"rotation",Vector3(rwing.rotation.x,rwing.rotation.y,deg_to_rad(-40)),0.5)
 	flap_tween.tween_callback(self.flap_up)
 
 func flap_up():
+	flapping = false
 	if flap_tween: flap_tween.cancel_free()
 	flap_tween = get_tree().create_tween()
-	flap_tween.tween_property(lwing,"rotation",Vector3(lwing.rotation.x,lwing.rotation.y,deg_to_rad(140)),0.25)
+	flap_tween.tween_property(lwing,"rotation",Vector3(lwing.rotation.x,lwing.rotation.y,deg_to_rad(140)),0.5)
 	flap_tween.set_parallel()
-	flap_tween.tween_property(rwing,"rotation",Vector3(rwing.rotation.x,rwing.rotation.y,deg_to_rad(40)),0.25)
+	flap_tween.tween_property(rwing,"rotation",Vector3(rwing.rotation.x,rwing.rotation.y,deg_to_rad(40)),0.5)
 	
 func _process(delta: float) -> void:
-	var v = self.velocity
-	var dir = Vector3(v.x,0,v.y)
+	if caught: return
+	self.rotate_y(self.rotate_speed*delta)
+	var v = Vector3(self.basis.z.x,0.0,self.basis.z.z).normalized() * SPEED
+	v.y = self.velocity.y
 	v += GRAVITY * delta
-	if self.position.y <= 3:
+	if !flapping and self.position.y < low_bound:
 		self.flap_down()
 		v += UP * delta * (0.25 + (1-randf()*0.5))
-	self.velocity = v
-	dir.rotated(Vector3(0,1,0),deg_to_rad(2))
-	self.velocity = Vector3(dir.x,v.y,dir.z)
-	self.move_and_collide(self.velocity)
+	self.velocity = v	
+	self.position += self.velocity * delta
+
+func catch():
+	if self.caught: return false
+	print("Butterfly Caught!",self.name)
+	self.caught = true
+	
--- root.gd
+++ root.gd
@@ -1,11 +1,59 @@
 extends Node3D
 
+var start_net_rot:Vector3
+var swiping:bool
+var swipe_ready:bool
 
+@onready var camera: Camera3D = get_viewport().get_camera_3d()
+
+var net_plane: Plane
+var caught_bug: Node3D
+
+func _ready():
+	start_net_rot = $NetArm.rotation
+	swiping = false
+	swipe_ready = true
+	net_plane = Plane(Vector3(0,1,0),$NetArm/Net.global_position)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.keycode == KEY_SPACE:
-			$AnimationPlayer.play("swipe")
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	$AnimationPlayer.play("ready")
+		if event.keycode == KEY_SPACE and swipe_ready:
+			swipe()
+		elif event.keycode == KEY_ESCAPE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+				
+func swipe():
+	swiping = true
+	swipe_ready = false
+	var tween = get_tree().create_tween()
+	tween.tween_property($NetArm,"rotation",Vector3(deg_to_rad(-90),0,0),0.15)
+	tween.tween_callback(self.finish_swipe)
+	
+func finish_swipe():
+	var tween = get_tree().create_tween()
+	tween.tween_property($NetArm,"rotation",start_net_rot,0.5)
+	tween.tween_callback(self.reset_swipe)
+	swiping = false
+	if caught_bug:
+		caught_bug.queue_free()
+		caught_bug = null
+	
+func reset_swipe():
+	swipe_ready = true
+			
+func _on_net_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	if swiping:
+		area.catch()
+		caught_bug = area
+	
+func _process(delta:float):
+	var mouse_pos:Vector2 = get_viewport().get_mouse_position()
+	var ray_origin: Vector3 = camera.project_ray_origin(mouse_pos)
+	var ray_normal: Vector3 = camera.project_ray_normal(mouse_pos)
+	var intersection = net_plane.intersects_ray(ray_origin,ray_normal)
+	if intersection:
+		$NetArm.position.x = intersection.x
+		$NetArm.position.z = intersection.z
+	if caught_bug:
+		caught_bug.global_position = $NetArm/Net/Ring.global_position
